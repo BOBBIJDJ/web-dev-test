8d28abd338491a7d482a92973acc3d62584a5814
BOBBIJDJ <bobbijdj23@outlook.com>
2023-02-08 22:06:58 +0100

    edits

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,5 @@
 color 0a
 echo off
 cls
-cd "c:/users/BOBBI/documents/local web server"
-start "C:\Program Files (x86)\Microsoft\Edge Dev\Application\msedge.exe" "localhost:6969"
+cd "c:/users/BOBBI/documents/web_dev_test"
 python -m http.server 6969
